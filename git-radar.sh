430d90f9071c3549a531e244bbd67f39e9369648
Thomas LÉVEIL <thomasleveil@users.noreply.github.com>
2016-11-16 22:19:38 +0100

    Delete git-radar.sh

--- git-radar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# install git-radar, see https://github.com/michaeldfallen/git-radar
-
-git clone https://github.com/michaeldfallen/git-radar ~/.git-radar
-cat >> ~/.bashrc <<-"EOF"
-	export PATH=$PATH:$HOME/.git-radar
-	export PS1="$PS1\$(git-radar --bash --fetch) "
-EOF
